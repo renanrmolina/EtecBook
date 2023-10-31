5d44feba174552374c61cea171ced62d00eb02ae
Renan Molina <renanrobertomolina@hotmail.com>
2023-10-31 20:32:26 -0300

    Criação do register

--- EtecBookUI/src/app/components/login/login.component.ts
+++ EtecBookUI/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){ }
+
+  ngOnInit(): void{
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(){
+    if (this.loginForm.valid){
+      console.log(this.loginForm.value);
+      //Enviar os dados a API
+    }else{
+      //Exibir uma mensagem erro
+      this.validateAllFormFields(this.loginForm);
+    }  
+  
+  }
+
+  private validateAllFormFields(formGroup: FormGroup){
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true})
+      } else if (control instanceof FormGroup){
+        this.validateAllFormFields(control)
+      }
+    });
+  }
 
 }
--- EtecBookUI/src/app/components/register/register.component.ts
+++ EtecBookUI/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  registerForm!: FormGroup;
+
+  constructor (private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
+
+  onSubmit(): void{
+    if (this.registerForm.valid){
+      //Enviar os dados para API
+      console.log(this.registerForm.value);
+    } else {
+      //Exibir Erro
+    }
+  }
+
+  get f(){
+    return this.registerForm.controls;
+  }
 
 }
